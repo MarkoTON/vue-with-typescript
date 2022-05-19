cee2d97e15bf0a13686ae2d8d7f3eca44ff8328a
Marko_TON <markotoncanovic@gmail.com>
2022-05-19 16:36:54 +0200

    checking const jobs = ref<Job[][]>

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,8 @@
     <button @click="changeName('Zelda')">change name</button>
     <button @click="changeAge('30')">change age</button> -->
     
-    <p>{{ jobs[0].location }}</p>
+    <p>{{ jobs[0].location }}</p>    
+    <!-- <p>{{ jobs[0][0].location }}</p> -->
   </div>
 </template>
 
@@ -38,6 +39,25 @@ export default defineComponent({
       { title: 'fisherman', location: 'lake hylia', salary: 21000, id: '4' },
       { title: 'prison guard', location: 'gerudo valley', salary: 32000, id: '5' }
     ])
+
+    //  const jobs = ref<Job[][]>(
+    //   [
+    //     [
+    //       { title: 'farm worker', location: 'lon lon ranch', salary: 30000, id: '1' },
+    //       { title: 'quarryman', location: 'death mountain', salary: 40000, id: '2' },
+    //       { title: 'flute player', location: 'the lost woods', salary: 35000, id: '3' },
+    //       { title: 'fisherman', location: 'lake hylia', salary: 21000, id: '4' },
+    //       { title: 'prison guard', location: 'gerudo valley', salary: 32000, id: '5' }
+    //     ],
+    //     [
+    //       { title: 'farm worker', location: 'lon lon ranch', salary: 30000, id: '1' },
+    //       { title: 'quarryman', location: 'death mountain', salary: 40000, id: '2' },
+    //       { title: 'flute player', location: 'the lost woods', salary: 35000, id: '3' },
+    //       { title: 'fisherman', location: 'lake hylia', salary: 21000, id: '4' },
+    //       { title: 'prison guard', location: 'gerudo valley', salary: 32000, id: '5' }
+    //     ]
+    //   ]
+    // )
 
     return { jobs }
   },
